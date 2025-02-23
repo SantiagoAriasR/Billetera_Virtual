09caf4db50c9381b0791d3b4c88508ad3dee676b
santiagooff <santiago.olarteb@uqvirtual.edu.co>
2025-02-23 17:38:32 -0500

    Se creo la carpeta "test" con las clases, se importo la dependencia de jUnit 5, se importo el plugin para ejecutar pruebas

---
+++ src/main/java/Test/BilleteraTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import models.Billetera;
+import models.Usuario;
+
+public class BilleteraTest {
+    Usuario usuario = new Usuario("Santiago","Calle 21", "123456","santiago@uqvirtual.edu.co", "123456", true);
+
+    @Test
+    public void consultarSaldoTest() {
+
+    }
+}
